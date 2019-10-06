ff73b6e252c4de25becb942a36e408206b2bcac5
Vidhi-D <vidhidhoka8@gmail.com>
2019-10-06 16:26:10 +0530

    Add notice Done

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import Login from './containers/Login'
 import { Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard';
-import DashboardIcon from '@material-ui/icons/Dashboard'
 import AddResident from './containers/AddResident';
 import AddGaurd from './containers/AddGaurd'
+import AddNotice from './containers/AddNotice'
+import PersonAdd from '@material-ui/icons/PersonAdd'
+import Security from '@material-ui/icons/Security'
+import Announcement from '@material-ui/icons/Announcement'
 
 class App extends Component {
 
   drawerList = {
-    'Add Resident': ['add-resident', <DashboardIcon />],
-    'Add Gaurd': ['add-gaurd', <DashboardIcon />],
+    'Add Resident': ['add-resident', <PersonAdd />],
+    'Add Gaurd': ['add-gaurd', <Security />],
+    'Add Notice': ['add-notice', <Announcement />],
   }
 
   render() {
@@ -22,6 +26,7 @@ class App extends Component {
           <Dashboard drawerList={this.drawerList}>
             <Route exact path='/login/dashboard/add-resident' component={AddResident} />
             <Route exact path='/login/dashboard/add-gaurd' component={AddGaurd} />
+            <Route exact path='/login/dashboard/add-notice' component={AddNotice} />
           </Dashboard>
         }
         />
